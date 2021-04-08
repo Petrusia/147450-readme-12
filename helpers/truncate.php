07966f507e33fa8936fcd6755383c9ef5017a3ab
Petrusia <petrasblazevicius1@gmail.com>
2021-04-08 17:44:44 +0300

    Поправил первую функцию

--- helpers/truncate.php
+++ helpers/truncate.php
@@ -1,12 +1,13 @@
 <?php
 
-/* функция, которая обрезает текстовое содержимое 
+/* функция, которая обрезает текстовое содержимое
 если оно превышает заданное число символов */
 
-function truncate($description, $lenght = 300) {
+function truncate($description, $lenght = 300)
+{
     $description = trim($description);
 
-    if(strlen($description) <= $lenght) {
+    if (strlen($description) <= $lenght) {
         return "<p>$description</p>";
     }
 
@@ -18,29 +19,29 @@ function truncate($description, $lenght = 300) {
     // разбил текст на отдельные слова (по пробелам)
     $description = explode(' ', $description);
 
-    //  в цикле последовательно считаю длину каждого слова и пробела 
-    foreach($description as $key => $word){
+    //  в цикле последовательно считаю длину каждого слова и пробела
+    foreach ($description as $key => $word) {
         $count += strlen($word) + 1; // + 1 это пробел
 
         /* останавливаю  цикл, когда суммарная длина символов
          в посчитанных словах начинает превышать ограничение */
-        if($count > $lenght) {
+        if ($count > $lenght) {
             // обрезаю до приемлемой длины
             $abbreviation = array_slice($description, 0, $key);
-            //  сложил  отдельные слова обратно в строку 
-            $description = implode(' ',$abbreviation);
+            //  сложил  отдельные слова обратно в строку
+            $description = implode(' ', $abbreviation);
             return "<p> $description $append </p> $read_more";
         }
     }
-     
-    //  другая версия 
+
+    //  другая версия
 
     // $description = substr($description, 0, $lenght);
-    // /*strrpos — Возвращает позицию последнего вхождения подстроки в строке 
-    //  у нас это пробел */ 
+    // /*strrpos — Возвращает позицию последнего вхождения подстроки в строке
+    //  у нас это пробел */
     // $position = strrpos($description, ' ');
     // $description = substr($description, 0, $position);
-    // return "<p> $description $append </p> $read_more";  
+    // return "<p> $description $append </p> $read_more";
 }
 
 
