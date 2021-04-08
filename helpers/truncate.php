e6050ec7a638551dc6528b530e5af80867769fae
Petrusia <petrasblazevicius1@gmail.com>
2021-04-08 23:15:09 +0300

    Как предложил Олег, и к ключу я добавил единицу, потому что массив отсчитывает от 0, а слова - от 1, вывод 283 символа, еще одно слово может поместиться.

--- helpers/truncate.php
+++ helpers/truncate.php
@@ -1,7 +1,7 @@
 <?php
 
-/* функция, которая обрезает текстовое содержимое
-если оно превышает заданное число символов */
+// функция, которая обрезает текстовое содержимое
+//если оно превышает заданное число символов
 
 function truncate($description, $length = 300): string
 {
@@ -16,6 +16,7 @@ function truncate($description, $length = 300): string
     $append = "&hellip;";
     $read_more = "<a class='post-text__more-link' href='#'>Читать далее</a>";
     $count = 0;
+    $content = ' ';
 
 
     // разбил текст на отдельные слова (по пробелам)
@@ -25,27 +26,21 @@ function truncate($description, $length = 300): string
     foreach ($description as $key => $word) {
         $count += mb_strlen($word) + 1; // + 1 это пробел
 
-        /* остановил  цикл, когда суммарная длина символов
-         в посчитанных словах начинает превышать ограничение */
+        //остановил  цикл, когда суммарная длина символов
+        // в посчитанных словах начинает превышать ограничение
         if ($count <= $length) {
             continue;
         }
-
+        // к ключу я добавил единицу,
+        //потому что массив отсчитывает от 0, а слова - от 1.
+        $key = $key + 1;
         // обрезал до приемлемой длины
         $abbreviation = array_slice($description, 0, $key);
         //  сложил  отдельные слова обратно в строку
-        $abbreviation = implode(' ', $abbreviation);
-        return sprintf('<p>%s%s</p>%s', $abbreviation, $append, $read_more);
+        $content = implode(" ", $abbreviation);
+        break;
     }
+    return sprintf('<p>%s%s</p>%s', $content, $append, $read_more);
 }
-//  другая версия
-
-// $description = substr($description, 0,  $length);
-// /*strrpos — Возвращает позицию последнего вхождения подстроки в строке
-//  у нас это пробел */
-// $position = strrpos($description, ' ');
-// $description = substr($description, 0, $position);
-// return "<p> $description $append </p> $read_more";
-
 
 
