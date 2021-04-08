74af9ea3c952d82b48920627bc14d6f81f5e0552
Petrusia <petrasblazevicius1@gmail.com>
2021-04-08 22:37:16 +0300

    Как предложил Олег

--- helpers/truncate.php
+++ helpers/truncate.php
@@ -3,18 +3,20 @@
 /* функция, которая обрезает текстовое содержимое
 если оно превышает заданное число символов */
 
-function truncate($description, $length = 300)
+function truncate($description, $length = 300): string
 {
     $description = trim($description);
 
     if (mb_strlen($description) <= $length) {
-        return "<p>$description</p>";
+        // return "<p>{$description}</p>";
+        return sprintf('<p>%s</p>', $description);
     }
 
 
     $append = "&hellip;";
     $read_more = "<a class='post-text__more-link' href='#'>Читать далее</a>";
     $count = 0;
+
 
     // разбил текст на отдельные слова (по пробелам)
     $description = explode(' ', $description);
@@ -23,25 +25,27 @@ function truncate($description, $length = 300)
     foreach ($description as $key => $word) {
         $count += mb_strlen($word) + 1; // + 1 это пробел
 
-        /* останавливаю  цикл, когда суммарная длина символов
+        /* остановил  цикл, когда суммарная длина символов
          в посчитанных словах начинает превышать ограничение */
-        if ($count > $length) {
-            // обрезаю до приемлемой длины
-            $abbreviation = array_slice($description, 0, $key);
-            //  сложил  отдельные слова обратно в строку
-            $description = implode(' ', $abbreviation);
-            return "<p> $description $append </p> $read_more";
+        if ($count <= $length) {
+            continue;
         }
+
+        // обрезал до приемлемой длины
+        $abbreviation = array_slice($description, 0, $key);
+        //  сложил  отдельные слова обратно в строку
+        $abbreviation = implode(' ', $abbreviation);
+        return sprintf('<p>%s%s</p>%s', $abbreviation, $append, $read_more);
     }
 }
-    //  другая версия
+//  другая версия
 
-    // $description = substr($description, 0,  $length);
-    // /*strrpos — Возвращает позицию последнего вхождения подстроки в строке
-    //  у нас это пробел */
-    // $position = strrpos($description, ' ');
-    // $description = substr($description, 0, $position);
-    // return "<p> $description $append </p> $read_more";
+// $description = substr($description, 0,  $length);
+// /*strrpos — Возвращает позицию последнего вхождения подстроки в строке
+//  у нас это пробел */
+// $position = strrpos($description, ' ');
+// $description = substr($description, 0, $position);
+// return "<p> $description $append </p> $read_more";
 
 
 
