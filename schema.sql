080cca13ee7da2415176985f9365ca3b4d2ae8c5
Petras <petrasblazevicius1@gmail.com>
2021-04-14 11:44:48 +0300

    Поправил по комментариям. Наверное в реале из db ничего не удаляется

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS user
 (
-    id                INT AUTO_INCREMENT PRIMARY KEY,
-    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    email             VARCHAR(128) NOT NULL UNIQUE,
-    login             VARCHAR(128) NOT NULL UNIQUE,
-    password          VARCHAR(128) NOT NULL,
-    avatar_url        TEXT
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    email      VARCHAR(128) NOT NULL UNIQUE,
+    login      VARCHAR(128) NOT NULL UNIQUE,
+    password   VARCHAR(128) NOT NULL,
+    avatar     TEXT
 );
 
 CREATE TABLE IF NOT EXISTS content_type
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS content_type
 CREATE TABLE IF NOT EXISTS post
 (
     id              INT AUTO_INCREMENT PRIMARY KEY,
-    creation_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     title           VARCHAR(128),
     content         TEXT,
     quote_author    VARCHAR(128),
@@ -29,11 +29,9 @@ CREATE TABLE IF NOT EXISTS post
     user_id         INT,
     content_type_id INT,
     FOREIGN KEY (user_id) REFERENCES user (id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
+        ON UPDATE CASCADE,
     FOREIGN KEY (content_type_id) REFERENCES content_type (id)
         ON UPDATE CASCADE
-        ON DELETE CASCADE
 );
 
 
@@ -46,25 +44,21 @@ CREATE TABLE IF NOT EXISTS comment
     user_id      INT,
     post_id      INT,
     FOREIGN KEY (user_id) REFERENCES user (id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
+        ON UPDATE CASCADE,
     FOREIGN KEY (post_id) REFERENCES post (id)
         ON UPDATE CASCADE
-        ON DELETE CASCADE
 );
 
 
-CREATE TABLE IF NOT EXISTS likes
+CREATE TABLE IF NOT EXISTS user_post_like_relation
 (
     id      INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     post_id INT,
     FOREIGN KEY (user_id) REFERENCES user (id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
+        ON UPDATE CASCADE,
     FOREIGN KEY (post_id) REFERENCES post (id)
         ON UPDATE CASCADE
-        ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS subscription
@@ -100,6 +94,18 @@ CREATE TABLE IF NOT EXISTS hashtag
     id   INT AUTO_INCREMENT PRIMARY KEY,
     name varchar(128) UNIQUE
 );
-CREATE FULLTEXT INDEX post_title_index ON post(title); # --https://dev.mysql.com/doc/refman/8.0/en/fulltext-search.html
-CREATE FULLTEXT INDEX comment_index ON comment(content); # --https://www.mysqltutorial.org/activating-full-text-searching.aspx/
+
+CREATE TABLE IF NOT EXISTS post_tag
+(
+    post_tag_id INT,
+    hashtag_id  INT,
+    FOREIGN KEY (post_tag_id) REFERENCES post (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (hashtag_id) REFERENCES hashtag (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+CREATE FULLTEXT INDEX post_title_index ON post (title); # --https://dev.mysql.com/doc/refman/8.0/en/fulltext-search.html
+CREATE FULLTEXT INDEX comment_index ON comment (content); # --https://www.mysqltutorial.org/activating-full-text-searching.aspx/
 
